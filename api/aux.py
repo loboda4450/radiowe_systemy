ba7fdc1e9d14f5dc2754c3536ee66ab3a7b3a269
loboda4450 <loboda4450@gmail.com>
2021-12-02 00:55:41 +0100

    register data validation

--- api/aux.py
+++ api/aux.py
@@ -49,20 +49,29 @@ async def get_current_active_user(current_user: User = Depends(get_current_user)
     return current_user
 
 
-# async def check_data(data: Register):
-#     if -180 < data.long < 180:
-#         raise HTTPException(status_code=406, detail=f'Data validation error: longitude {data.long} exceeds limit [-180; 180]')
-#     if -90 < data.lat < 90:
-#         raise HTTPException(status_code=406, detail=f'Data validation error: latitude {data.lat} exceeds limit [-90; 90]')
-#     if 0 < data.nf < 40:
-#         raise HTTPException(status_code=406, detail=f'Data validation error: noise factor {data.nf} exceeds limit [0; 40]')
-#     if 0 < data.ptx < 40:
-#         raise HTTPException(status_code=406, detail=f'Data validation error: ptx {data.ptx} exceeds limit [0; 40]')
-#     if 0 < data.gt < 40:
-#         raise HTTPException(status_code=406, detail=f'Data validation error: gain transmitter {data.gt} exceeds limit [0; 40]')
-#     if 0 < data.gr < 40:
-#         raise HTTPException(status_code=406, detail=f'Data validation error: gain receiver {data.gr} exceeds limit [0; 40]')
-#     if 0 < data.channel < 12:
-#         raise HTTPException(status_code=406, detail=f'Data validation error: channel {data.channel} exceeds limit [0; 12]')
-#
-#     return data
+async def check_data(data: Register):
+    if not -180 <= data.long <= 180:
+        raise HTTPException(status_code=406, detail=f'Data validation error: longitude {data.long} exceeds limit ['
+                                                    f'-180; 180]')
+    if not -90 <= data.lat <= 90:
+        raise HTTPException(status_code=406, detail=f'Data validation error: latitude {data.lat} exceeds limit [-90; 90]')
+    if not 0 <= data.nf <= 40:
+        raise HTTPException(status_code=406, detail=f'Data validation error: noise factor {data.nf} exceeds limit [0; '
+                                                    f'40]')
+    if not 0 <= data.ptx <= 40:
+        raise HTTPException(status_code=406, detail=f'Data validation error: ptx {data.ptx} exceeds limit [0; 40]')
+    if not 0 <= data.gt <= 40:
+        raise HTTPException(status_code=406, detail=f'Data validation error: gain transmitter {data.gt} exceeds limit '
+                                                    f'[0; 40]')
+    if not 0 <= data.gr <= 40:
+        raise HTTPException(status_code=406, detail=f'Data validation error: gain receiver {data.gr} exceeds limit ['
+                                                    f'0; 40]')
+    if not 0 <= data.channel <= 12:
+        raise HTTPException(status_code=406, detail=f'Data validation error: channel {data.channel} exceeds limit [0; '
+                                                    f'12]')
+    if not 0 <= data.aclr1 <= 40:
+        raise HTTPException(status_code=406, detail=f'Data validation error: aclr1 {data.aclr1} exceeds limit [0; 40]')
+    if not 0 <= data.aclr2 <= 40:
+        raise HTTPException(status_code=406, detail=f'Data validation error: aclr2 {data.aclr2} exceeds limit [0; 40]')
+
+    return data
